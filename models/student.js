c9cc580c36db5d311d397dc038be655748354dd6
Emikoya <pruvost.lily@gmail.com>
2023-03-21 15:20:23 +0100

    modifications

--- models/student.js
+++ models/student.js
@@ -1,33 +1,50 @@
-// (Étape 1) Import de express
-var express = require('express');
+// (Étape 1) Import du DRM mongoose et luxon
+var mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
-// (Étape 1) Définition du router
-var router = express.Router();
+// (Étape 2) Définition de la méthode qui permet de valider le format d'un email
+const validateEmail = function (email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
 
-// (Étape 2) Ajout de la route qui permet d'ajouter un étudiant
-router.post("/", (req, res) => {
-    return res.status(201).json("CREATE !");
+// (Étape 2) Définition du schéma student
+// https://mongoosejs.com/docs/guide.html
+// https://mongoosejs.com/docs/schematypes.html#schematype-options
+const studentSchema = new mongoose.Schema({
+  _id: { type: Number, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  class: { type: String, required: true, enum: ["LP-DIM-FI", "LP-DIM-APP"] },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: true,
+    validate: [validateEmail, "Please fill a valid email address"],
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+    transform: (x) => DateTime.fromJSDate(x).toISODate(),
+  },
 });
 
-// (Étape 2) Ajout de la route qui permet d'afficher tous les étudiants
-router.get("/", (req, res) => {
-    return res.status(200).json("GET ALL!");
+// (Étape 3) Création d'une nouvelle propriété virtuelle "id" qui aura la valeur de la propriété "_id"
+studentSchema.virtual("id").get(function () {
+  return this._id;
 });
 
-// (Étape 2) Ajout de la route qui permet d'afficher un seul étudiant grâce à son identifant
-router.get("/:id", (req, res) => {
-    return res.status(200).json("GET BY ID = !");
+// (Étape 3) Définition de l'object qui sera retourné lorsque la méthode toJSON est appelée
+studentSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
 });
 
-// (Étape 2) Ajout de la route qui permet de modifier un seul étudiant grâce à son identifant
-router.put("/:id", (req, res) => {
-    return res.status(200).json("UPDATE !");
-});
-
-// (Étape 2) Ajout de la route qui permet de supprimer un seul étudiant grâce à son identifant
-router.delete("/:id", (req, res) => {
-    return res.status(200).json("DELETE !");
-});
-
-// (Étape 1) Export du router
-module.exports = router;
+// (Étape 4) Export du modèle student
+// Les modèles sont responsables de la création et de la lecture des documents à partir de la base de données MongoDB.
+module.exports = mongoose.model("students", studentSchema);
